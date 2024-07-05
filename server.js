094f915d90ad54b4f21f9b6e9a59eb21af9b8e87
fredrick.hem <fredrick.hem@rubicotech.in>
2024-07-05 14:15:15 +0530

    add: server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+// Import the http module
+const http = require('http');
+
+// Define the hostname and port where the server will listen
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create the server
+const server = http.createServer((req, res) => {
+  // Set the response HTTP headers
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  
+  // Send the response body "Hello World"
+  res.end('Helloo World\n');
+});
+
+// Make the server listen on the specified hostname and port
+server.listen(port, hostname, () => {
+  console.log(`Server is running in http://${hostname}:${port}/`);
+});
